94ec19629c78e1038f0761d44c3a5ffdf24c3c97
dimachilaia <chilaiadima1@gmail.com>
2022-11-24 01:57:12 +0400

    created editperson component

---
+++ src/Edit/EditPerson.jsx
@@ -0,0 +1,151 @@
+import styled from "styled-components";
+import useStore from "../store";
+import { Button,ModalBody} from "reactstrap";
+
+const EditPerson = ({
+  setChange,
+  setName,
+  email,
+  setEmail,
+  setGender,
+  gender,
+  street,
+  setStreet,
+  city,
+  setCity,
+  phone,
+  setPhone,
+  id,
+  name, 
+}) => {
+  const updatePerson = useStore((state) => state.updatePerson);
+
+  const handleEdit = (response) => {
+    updatePerson({
+      name: response.name,
+      email: response.email,
+      phone: response.phone,
+      street: response.street,
+      city: response.city,
+      gender: response.gender,
+      id: response.id,
+    });
+    setChange(false);
+  };
+
+  return (
+    <div>
+      <BlackBg onClick={() => setChange(false)}></BlackBg>
+      <ChangeModal>
+        <ModalBody>
+          <FormGroup>
+            <Input
+              id="label"
+              value={name}
+              placeholder="Enter Your Name"
+              onChange={(e) => setName(e.target.value)}
+            />
+            <Input
+              id="label"
+              value={email}
+              placeholder="Enter Your Email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <Select
+              onChange={(e) => setGender(e.target.value)}
+              name="gender"
+              value={gender}
+            >
+              <option value="Not selected">Select Gender</option>
+              <option value="Male">Male</option>
+              <option value="Female">Female</option>
+              <option value="Other">other</option>
+            </Select>
+            <Input
+              id="label"
+              value={street}
+              placeholder="Enter Your Street Address"
+              style={{ marginTop: "5px" }}
+              onChange={(e) => setStreet(e.target.value)}
+            />
+            <Input
+              id="label"
+              value={city}
+              placeholder="Enter Your City Address"
+              onChange={(e) => setCity(e.target.value)}
+            />
+            <Input
+              id="label"
+              placeholder="Enter Your Phone"
+              type="tel"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+            />
+            <Button
+              onClick={() =>
+                handleEdit({
+                  name,
+                  email,
+                  phone,
+                  street,
+                  city,
+                  gender,
+                  id,
+                })
+              }
+            >
+              Edit
+            </Button>
+          </FormGroup>
+        </ModalBody>
+      </ChangeModal>
+    </div>
+  );
+};
+
+export default EditPerson;
+
+const ChangeModal = styled.div`
+  position: absolute;
+  width: 50%;
+  height: 400px;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: white;
+  border-radius: 10px;
+  z-index: 3;
+`;
+
+const BlackBg = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: 2;
+  background: rgba(0, 0, 0, 0.5);
+`;
+
+const FormGroup = styled.form`
+  color: palevioletred;
+  display: block;
+  width: 300px;
+  margin: 50px auto;
+`;
+
+const Input = styled.input`
+  padding: 0.5em;
+  background: papayawhip;
+  border: none;
+  border-radius: 3px;
+  width: 100%;
+  margin-bottom: 0.5em;
+`;
+
+const Select = styled.select`
+  width: 300px;
+  background-color: papayawhip;
+  height: 45px;
+  outline: none;
+`;
--- src/FetchData.jsx
+++ src/FetchData.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import useStore from "./store";
 import styled from "styled-components";
 import Loading from "./Loading/Loading";
 import DataTable from "react-data-table-component";
 import ModalComponent from "./Modal/ModalComponent";
+import EditPerson from "./Edit/EditPerson";
 
 const FetchData = () => {
   const data = useStore((state) => state.data);
@@ -13,7 +13,6 @@ const FetchData = () => {
   const fetchData = useStore((state) => state.fetch);
   const removeStudent = useStore((state) => state.removePerson);
   const addPerson = useStore((state) => state.addPerson);
-  const updatePerson = useStore((state) => state.updatePerson);
   const [change, setChange] = useState(false);
   const [modal, setModal] = useState(false);
 
@@ -23,7 +22,7 @@ const FetchData = () => {
   const [city, setCity] = useState("");
   const [phone, setPhone] = useState("");
   const [gender, setGender] = useState("Not selected");
-  const [id, setId] = useState()
+  const [id, setId] = useState();
 
   const deleteHandler = (id) => {
     removeStudent(id);
@@ -40,19 +39,6 @@ const FetchData = () => {
         gender: response.gender,
       });
     }
-  };
-
-  const handleEdit = (response) => {
-    updatePerson({
-      name: response.name,
-      email: response.email,
-      phone: response.phone,
-      street: response.street,
-      city: response.city,
-      gender: response.gender,
-      id: response.id,
-    });
-    setChange(false);
   };
 
   useEffect(() => {
@@ -114,7 +100,7 @@ const FetchData = () => {
     setPhone(row.phone);
     setGender(row.gender);
     setChange(true);
-    setId(row.id)
+    setId(row.id);
   };
 
   return (
@@ -132,73 +118,25 @@ const FetchData = () => {
         />
       }
       {change && (
-        <>
-          <BlackBg onClick={() => setChange(false)}></BlackBg>
-          <ChangeModal>
-            <ModalBody>
-              <FormGroup>
-                <Input
-                  id="label"
-                  value={name}
-                  placeholder="Enter Your Name"
-                  onChange={(e) => setName(e.target.value)}
-                />
-                <Input
-                  id="label"
-                  value={email}
-                  placeholder="Enter Your Email"
-                  onChange={(e) => setEmail(e.target.value)}
-                />
-                <Select
-                  onChange={(e) => setGender(e.target.value)}
-                  name="gender"
-                  value={gender}
-                >
-                  <option value="Not selected">Select Gender</option>
-                  <option value="Male">Male</option>
-                  <option value="Female">Female</option>
-                  <option value="Other">other</option>
-                </Select>
-                <Input
-                  id="label"
-                  value={street}
-                  placeholder="Enter Your Street Address"
-                  style={{ marginTop: "5px" }}
-                  onChange={(e) => setStreet(e.target.value)}
-                />
-                <Input
-                  id="label"
-                  value={city}
-                  placeholder="Enter Your City Address"
-                  onChange={(e) => setCity(e.target.value)}
-                />
-                <Input
-                  id="label"
-                  placeholder="Enter Your Phone"
-                  type="tel"
-                  value={phone}
-                  onChange={(e) => setPhone(e.target.value)}
-                />
-                <Button
-                  onClick={() =>
-                    handleEdit({
-                      name,
-                      email,
-                      phone,
-                      street,
-                      city,
-                      gender,
-                      id
-                    })
-                  }
-                >
-                  Edit
-                </Button>
-              </FormGroup>
-            </ModalBody>
-          </ChangeModal>
-        </>
-      ) }
+        <EditPerson
+          email={email}
+          setEmail={setEmail}
+          name={name}
+          setName={setName}
+          phone={phone}
+          setPhone={setPhone}
+          city={city}
+          setCity={setCity}
+          street={street}
+          setStreet={setStreet}
+          gender={gender}
+          setGender={setGender}
+          id={id}
+          setId={setId}
+          change={change}
+          setChange={setChange}
+        />
+      )}
     </div>
   );
 };
@@ -218,50 +156,4 @@ const ButtonElement = styled.button`
     opacity: 0.5;
     transition: 0.5s;
   }
-`;
-
-const ChangeModal = styled.div`
-  position: absolute;
-  width: 50%;
-  height: 400px;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  background: white;
-  border-radius:10px;
-  z-index: 3;
-`;
-
-const BlackBg = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100vw;
-  height: 100vh;
-  z-index: 2;
-  background: rgba(0, 0, 0, 0.5);
-`;
-
-
-const FormGroup = styled.form`
-  color: palevioletred;
-  display: block;
-  width: 300px;
-  margin: 50px auto;
-`;
-
-const Input = styled.input`
-  padding: 0.5em;
-  background: papayawhip;
-  border: none;
-  border-radius: 3px;
-  width: 100%;
-  margin-bottom: 0.5em;
-`;
-
-const Select = styled.select`
-  width: 300px;
-  background-color: papayawhip;
-  height: 45px;
-  outline: none;
 `;
